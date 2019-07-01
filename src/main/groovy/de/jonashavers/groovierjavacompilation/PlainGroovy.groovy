7af5c123bfbdcdc0d6d1f3c100293a50639578b3
Victor Herasme <victor.herasme@koedia.com>
2019-07-01 16:20:44 +0200

    Change config for java11

--- src/main/groovy/de/jonashavers/groovierjavacompilation/PlainGroovy.groovy
+++ src/main/groovy/de/jonashavers/groovierjavacompilation/PlainGroovy.groovy
@@ -2,4 +2,9 @@ package de.jonashavers.groovierjavacompilation
 
 class PlainGroovy {
     String language = 'groovy'
+
+    static void main(String[] args) {
+        String lang = "Groovy"
+        println(lang)
+    }
 }
